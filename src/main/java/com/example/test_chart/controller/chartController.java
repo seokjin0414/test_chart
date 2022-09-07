2f57cf1314b98512b0ef4e10093fd1a19dfc282f
seokjin shin <sars21@hanmail.net>
2022-09-08 00:41:39 +0900

    thymeleaf 설정

--- src/main/java/com/example/test_chart/controller/chartController.java
+++ src/main/java/com/example/test_chart/controller/chartController.java
@@ -1,6 +1,5 @@
 package com.example.test_chart.controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/test_chart/controller/homeController.java
+++ src/main/java/com/example/test_chart/controller/homeController.java
@@ -2,6 +2,7 @@ package com.example.test_chart.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class homeController {
 
     @GetMapping("/")
-    public String goHome() {
+    public String goHome(Model model) {
+
+        model.addAttribute("hello", "서버에서 보내준 값입니다");
         return "home";
 
     }
