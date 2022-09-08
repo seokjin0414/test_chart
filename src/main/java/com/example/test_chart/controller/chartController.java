652b27ec6dcb72334e61d5448766821fe4591c1b
seokjin shin <sars21@hanmail.net>
2022-09-09 00:03:32 +0900

    chart.js 출력 확인 up bit api  key생성 request 요청 + 값 get

--- src/main/java/com/example/test_chart/controller/chartController.java
+++ src/main/java/com/example/test_chart/controller/chartController.java
@@ -1,9 +1,21 @@
 package com.example.test_chart.controller;
 
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.UUID;
 
 @Slf4j
 @Controller
@@ -14,4 +26,42 @@ public class chartController {
     public String chartGo() {
         return "chart/test";
     }
+
+    @GetMapping("/test1")
+    public String getCoin() {
+
+
+        String accessKey = "발급받은 Access key";
+        String secretKey = "발급받은 Secret key";
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        String jwtToken = JWT.create()
+                .withClaim("access_key", accessKey)
+                .withClaim("nonce", UUID.randomUUID().toString())
+                .sign(algorithm);
+
+        String authenticationToken = "Bearer " + jwtToken;
+try {
+    OkHttpClient client = new OkHttpClient();
+
+    Request request = new Request.Builder()
+            .url("https://api.upbit.com/v1/trades/ticks?market=USDT-BTC&count=1")
+            .get()
+            .addHeader("Accept", "application/json")
+            .build();
+
+    Response response = client.newCall(request).execute();
+    System.out.println(response);
+
+} catch (IOException e) {
+    e.printStackTrace();
+}
+
+
+
+        return "chart/test";
+    }
+
+
 }
--- src/main/java/com/example/test_chart/service/chartService.java
+++ src/main/java/com/example/test_chart/service/chartService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class chartService {
+
 }
